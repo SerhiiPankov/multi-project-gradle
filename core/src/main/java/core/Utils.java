5ad2b6806f6f8248ebe472efbe04065c4f15e607
PankovSerhii <pankoff.serg@gmail.com>
2023-06-29 06:49:43 +0300

    fix bag with stream

--- core/src/main/java/core/Utils.java
+++ core/src/main/java/core/Utils.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(String... str) {
-        return !Arrays.stream(str).map(StringUtils::isPositiveNumber)
-                .toList().contains(false);
+        return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
 }
